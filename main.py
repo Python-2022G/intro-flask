8d5f4d5bd721a2c0f45010c8bfb3f6aaa5d4f5b3
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-14 15:41:02 +0500

    routes;

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
+@app.route('/about')
+def about():
+    return 'About!'
+
+@app.route('/contact')
+def contact():
+    return 'Contact!'
+
 if __name__ == '__main__':
     app.run(debug=True)
